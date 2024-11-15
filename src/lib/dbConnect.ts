2f7039afabcbb730774eb84e37d729a96574fce2
Kr1shnendu <majikrishnendu1@gmail.com>
2024-11-15 06:10:36 +0530

    mongo fixes

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
-
 const dbConnect = async () => {
     const connectionState = mongoose.connection.readyState;
 
@@ -19,7 +17,7 @@ const dbConnect = async () => {
         const opts = {
             bufferCommands: true,
         };
-        await mongoose.connect(MONGO_URI!, opts);
+        await mongoose.connect(process.env.MONGO_URI!, opts);
         console.log("db connected");
     } catch (error: any) {
         console.log("Error: ",error);
